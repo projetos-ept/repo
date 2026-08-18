0845f84b5a98390e3510e18d2d9d8448ddee7d85
projetos-ept <lucas.batista.eduk@gmail.com>
2026-08-18 07:13:41 -0300

    Add configuration for API and site details

---
+++ docs/config.js
@@ -0,0 +1,7 @@
+// Edite somente esta URL depois de publicar o Worker.
+// Não coloque senha, token ou chave do Cloudflare neste arquivo público.
+window.REPO_CONFIG = {
+    API_URL: "https://repositorio-api.lucas-batista-biomedico.workers.dev",
+    SITE_NAME: "Repositório CETEP/LNAB",
+    SITE_SUBTITLE: "Documentos e materiais para download"
+  };
